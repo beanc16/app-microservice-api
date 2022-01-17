eafb031b166a607a07ce41630ac350eadce226c0
beanc16 <bean.christopher@outlook.com>
2022-01-17 14:49:02 -0500

    Update get apps schema

--- src/api/v1/public/validation/schemas/apps/get.js
+++ src/api/v1/public/validation/schemas/apps/get.js
@@ -1,22 +1,14 @@
 const Joi = require("joi");
 const { JoiRequired } = require("../../../../../../../custom_modules/JoiHelpers");
-const appSchemas = require("./getHelpers");
+const getAppSchemaHelpers = require("./getHelpers");
 
 
 
 
-// Get many apps
-const getAppsSchema = appSchemas.getByEnvOrSearchName;
-
-// Get one app
-const getAppSchema = JoiRequired.alternatives().try(
-    appSchemas.getByEnvAndSearchName,
-    appSchemas.getById,
-);
+const getAppsSchema = getAppSchemaHelpers.getByIdOrEnvOrSearchName;
 
 
 
 module.exports = {
     getAppsSchema,
-    getAppSchema,
 };
