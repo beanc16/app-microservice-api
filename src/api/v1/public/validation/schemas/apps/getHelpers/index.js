6c4c055cebea528973b4f02841632e066ed1aebb
beanc16 <bean.christopher@outlook.com>
2022-01-17 14:49:33 -0500

    Update get apps validators

--- src/api/v1/public/validation/schemas/apps/getHelpers/index.js
+++ src/api/v1/public/validation/schemas/apps/getHelpers/index.js
@@ -1,15 +1,17 @@
-const getById = require("./getById");
 const getByEnv = require("./getByEnv");
 const getByEnvAndSearchName = require("./getByEnvAndSearchName");
 const getByEnvOrSearchName = require("./getByEnvOrSearchName");
+const getById = require("./getById");
+const getByIdOrEnvOrSearchName = require("./getByIdOrEnvOrSearchName");
 const getBySearchName = require("./getBySearchName");
 
 
 
 module.exports = {
-    getById,
     getByEnv,
     getByEnvAndSearchName,
     getByEnvOrSearchName,
+    getById,
+    getByIdOrEnvOrSearchName,
     getBySearchName,
 };
