f78642c020a2b1d146e6c361563de5e5dc757050
beanc16 <bean.christopher@outlook.com>
2022-01-17 20:22:49 -0500

    Add validation for app IDs

--- src/api/v1/public/validation/schemas/apps/universalHelpers/apps/index.js
+++ src/api/v1/public/validation/schemas/apps/universalHelpers/apps/index.js
@@ -3,6 +3,7 @@ const envsArray = require("./envsArray");
 const searchNameStrings = require("./searchNameString");
 const displayNameString = require("./displayNameString");
 const dataObjs = require("./dataObj");
+const ids = require("./id");
 
 
 
@@ -11,5 +12,5 @@ module.exports = {
     ...envsArray,
     ...searchNameStrings,
     ...displayNameString,
-    ...dataObjs,
+    ...ids,
 };
