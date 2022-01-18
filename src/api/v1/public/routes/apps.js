b88abc1fdc572da04f23ea039582a6bf7c522b6b
beanc16 <bean.christopher@outlook.com>
2022-01-17 20:48:30 -0500

    Add endpoint for testing create app validation

--- src/api/v1/public/routes/apps.js
+++ src/api/v1/public/routes/apps.js
@@ -7,6 +7,12 @@ const express = require("express");
 const app = express();
 
 
+// Access req.body in post requests
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());                         // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
 // Controllers
 const { AppController } = require("../../../../js/controllers");
 
@@ -14,6 +20,7 @@ const { AppController } = require("../../../../js/controllers");
 // Validation
 const {
     validateGetAppsPayload,
+    validateCreateAppPayload
 } = require("../validation");
 
 
@@ -61,6 +68,7 @@ app.get("/", function(req, res)
     })
     .catch(function (err)
     {
+        console.log("err:", err);
         const errResponse = new ValidationErrorResponse({
             error: err,
             res: res,
@@ -125,15 +133,49 @@ function convertEnvForFindParams(query)
  * POSTS *
  *********/
 
-/*
 app.post("/", function(req, res)
 {
-    const response = new SuccessResponse({
-        message: "Pong",
+    validateCreateAppPayload(req.body)
+    .then(function (payload)
+    {
+        const response = new SuccessResponse({
+            res,
+            message: "Successfully validated payload",
+        });
+        res.send(response);
+        /*
+        const findParams = convertEnvForFindParams(req.query);
+
+        AppController.getAll(findParams)
+        .then(function (data)
+        {
+            const response = new SuccessResponse({
+                res,
+                message: getSuccessMessageForGetApps(req.query),
+                data: data.results,
+            });
+            res.send(response);
+        })
+        .catch(function (err)
+        {
+            const errResponse = new BadRequestErrorResponse({
+                res,
+                message: "Failed to retrieve all apps",
+                err,
+            });
+            res.send(errResponse);
+        });
+        */
+    })
+    .catch(function (err)
+    {
+        const errResponse = new ValidationErrorResponse({
+            error: err,
+            res: res,
+        });
+        res.send(errResponse);
     });
-    res.send(response);
 });
-*/
 
 
 
