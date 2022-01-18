79d335d7572b953d4e7059998796a862081ada4a
beanc16 <bean.christopher@outlook.com>
2022-01-17 20:31:35 -0500

    Fix import and export schema bug

--- src/api/v1/public/validation/schemas/apps/universalHelpers/apps/index.js
+++ src/api/v1/public/validation/schemas/apps/universalHelpers/apps/index.js
@@ -1,16 +1,17 @@
+const ids = require("./id");
 const envStrings = require("./envString");
 const envsArray = require("./envsArray");
 const searchNameStrings = require("./searchNameString");
 const displayNameString = require("./displayNameString");
 const dataObjs = require("./dataObj");
-const ids = require("./id");
 
 
 
 module.exports = {
+    ...ids,
     ...envStrings,
     ...envsArray,
     ...searchNameStrings,
     ...displayNameString,
-    ...ids,
+    ...dataObjs,
 };
