ee29c802960ed9b7fc18e03c8d9efa257f601069
beanc16 <bean.christopher@outlook.com>
2022-01-17 20:12:03 -0500

    Create validation for app data

--- src/api/v1/public/validation/schemas/apps/universalHelpers/index.js
+++ src/api/v1/public/validation/schemas/apps/universalHelpers/index.js
@@ -2,6 +2,7 @@ const envStrings = require("./envString");
 const envsArray = require("./envsArray");
 const searchNameStrings = require("./searchNameString");
 const displayNameString = require("./displayNameString");
+const dataObjs = require("./dataObj");
 
 
 
@@ -10,4 +11,5 @@ module.exports = {
     ...envsArray,
     ...searchNameStrings,
     ...displayNameString,
+    ...dataObjs,
 };
