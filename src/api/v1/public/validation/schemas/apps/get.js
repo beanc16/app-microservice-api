f8743dc4a15bbc91881dd19bc424f65b1a262ec7
beanc16 <bean.christopher@outlook.com>
2022-01-17 20:27:56 -0500

    Update getAppsSchema

--- src/api/v1/public/validation/schemas/apps/get.js
+++ src/api/v1/public/validation/schemas/apps/get.js
@@ -1,11 +1,33 @@
 const Joi = require("joi");
 const { JoiRequired } = require("../../../../../../../custom_modules/JoiHelpers");
 const getAppSchemaHelpers = require("./getHelpers");
+const appSchemas = require("./universalHelpers").appSchemas;
 
 
 
+// Get apps - by id, env, or searchName
+const getAppsSchema = JoiRequired.alternatives().try(
+    // By id
+    JoiRequired.object({
+        id: appSchemas.idStringRequired,
+        env: appSchemas.envString,
+        searchName: appSchemas.searchName,
+    }),
 
-const getAppsSchema = getAppSchemaHelpers.getByIdOrEnvOrSearchName;
+    // By env
+    JoiRequired.object({
+        id: appSchemas.idString,
+        env: appSchemas.envStringRequired,
+        searchName: appSchemas.searchName,
+    }),
+
+    // By searchName
+    JoiRequired.object({
+        id: appSchemas.idString,
+        env: appSchemas.envString,
+        searchName: appSchemas.searchNameStringRequired,
+    }),
+);
 
 
 
