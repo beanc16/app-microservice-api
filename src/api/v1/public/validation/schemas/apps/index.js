6c84b919c3cdf77c8b61cc27687831e739f6a375
beanc16 <bean.christopher@outlook.com>
2022-01-22 07:36:02 -0500

    Export app update validation function

--- src/api/v1/public/validation/schemas/apps/index.js
+++ src/api/v1/public/validation/schemas/apps/index.js
@@ -1,9 +1,11 @@
 const getSchemas = require("./get");
 const createSchemas = require("./create");
+const updateSchemas = require("./update");
 
 
 
 module.exports = {
     ...getSchemas,
     ...createSchemas,
+    ...updateSchemas,
 };
