ab26ce454a9602ff375ebb407cf39e1e5001dd99
beanc16 <bean.christopher@outlook.com>
2022-01-22 07:35:41 -0500

    Export app update validation schema

--- src/api/v1/public/validation/validators/apps/index.js
+++ src/api/v1/public/validation/validators/apps/index.js
@@ -1,9 +1,11 @@
 const getValidators = require("./get");
 const createValidators = require("./create");
+const updateValidators = require("./update");
 
 
 
 module.exports = {
     ...getValidators,
     ...createValidators,
+    ...updateValidators,
 };
