1f3118c2395acef97a3632b913960d592225e630
beanc16 <bean.christopher@outlook.com>
2022-01-22 09:42:58 -0500

    Add validation schema for deleting an app

--- src/api/v1/public/validation/schemas/apps/index.js
+++ src/api/v1/public/validation/schemas/apps/index.js
@@ -1,6 +1,7 @@
 const getSchemas = require("./get");
 const createSchemas = require("./create");
 const updateSchemas = require("./update");
+const deleteSchemas = require("./delete");
 
 
 
@@ -8,4 +9,5 @@ module.exports = {
     ...getSchemas,
     ...createSchemas,
     ...updateSchemas,
+    ...deleteSchemas,
 };
