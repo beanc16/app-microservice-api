1a81c38f8f723e427df8da958a9263bba792c15e
beanc16 <bean.christopher@outlook.com>
2022-01-30 17:01:12 -0500

    Swap JoiHelpers module for @beanc16/joi-helpers

--- src/api/v1/public/validation/validators/apps/get.js
+++ src/api/v1/public/validation/validators/apps/get.js
@@ -1,5 +1,5 @@
 const { getAppsSchema } = require("../../schemas");
-const { validateJoiSchema } = require("../../../../../../../custom_modules/JoiHelpers");
+const { validateJoiSchema } = require("@beanc16/joi-helpers");
 
 
 
